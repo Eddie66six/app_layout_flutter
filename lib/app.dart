8fcb955b2453dda5b7b2eeb028725822b7806767
Eddie <jogos.coisas@gmail.com>
2018-12-05 15:55:19 -0200

    infinity scroll

--- lib/app.dart
+++ lib/app.dart
@@ -6,85 +6,51 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
-  var listX = <double>[];
+  ScrollController controller;
+  var items = <int>[];
   @override
-  void initState() {
+  initState() {
     super.initState();
+    controller = new ScrollController();
+    controller.addListener((){
+      double maxScroll = controller.position.maxScrollExtent;
+      double currentScroll = controller.position.pixels;
+      double delta = 200.0;
+      if ( maxScroll - currentScroll <= delta) {
+        var last = items[items.length-1];
+        setState(() {
+          for (var i = last; i <= last + 10; i++) {
+            items.add(i);
+          }
+        });
+      }
+    });
+
+    for (var i = 0; i <= 30; i++) {
+      items.add(i);
+    }
+  }
+
+  @override
+  dispose() {
+    controller.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    var size = MediaQuery.of(context).size;
-    var objSize = new Size(size.width -50, 100);
-    var top = size.height/2 - objSize.height/2;
-    var total = 2;
-    var lstObjs = <Widget>[];
-    if(lstObjs.length == 0){
-      for (var i = 0; i < total; i++) {
-        if(listX.length < total)
-          listX.add(i == 0 ? 25 : objSize.width * i + 50); 
-        lstObjs.add(
-          new Card(objSize, i, listX[i], top)
-        );
-      }
-    }
-
-    _updatePositions(double qtd){
-      //minimo
-      if(qtd > 0 && listX[0] > 25){
-        return;
-      }
-      //maximo
-      if(qtd < 0 && listX[listX.length-1] <= 25){
-        return;
-      }
-      for (var i = 0; i < listX.length; i++) {
-        listX[i] += qtd;
-      }
-    }
-
-    return Scaffold(
+    return new Scaffold(
       body: new Center(
-        child: new GestureDetector(
-          child: new Stack(
-            overflow: Overflow.clip,
-            children: lstObjs,
+        child: new Container(
+          child: new ListView.builder(
+              controller: controller,
+              itemCount: items.length,
+                itemBuilder: (BuildContext ctxt, int index) {
+                return new Text(items[index].toString(), style: new TextStyle(fontSize: 24));
+            }
           ),
-          onPanUpdate: (DragUpdateDetails d){
-            setState(() {
-               _updatePositions(d.delta.dx);           
-            });
-          },
-        )
+        ),
       )
-    );
-  }
-}
-
-class Card extends StatefulWidget {
-  Card(this.size,this.index, this.positionX, this.positionY);
-  double positionX;
-  double positionY;
-  final Size size;
-  final int index;
-  @override
-  _CardState createState() => _CardState();
-}
-
-class _CardState extends State<Card> {
-  @override
-  Widget build(BuildContext context) {
-    return new Positioned(
-          left: widget.positionX,
-          top: widget.positionY,
-          child: new Container(
-            decoration: new BoxDecoration(
-              color: Colors.red,
-              border: new Border.all()
-            ),
-            height: widget.size.height,
-            width: widget.size.width,
-          ),
     );
   }
 }
